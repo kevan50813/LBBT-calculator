519cde2bc562c8b1f1a0e2b916950c4fe29671d0
kevan50813 <kevanjordan9@gmail.com>
2022-05-19 18:03:13 +0100

    started progrmaing for BJSS code -test

---
+++ src/PriceBand.java
@@ -0,0 +1,9 @@
+//enum for price bands
+//information form https://www.gov.uk/stamp-duty-land-tax/residential-property-rates
+public enum PriceBand {
+    BAND_A, // <= 125k
+    BAND_B, // 125,001 - 250k
+    BAND_C, // 250,001 - 325k
+    BAND_D, // 325,001 - 750k
+    BAND_E  // >= 750,001
+}
---
+++ src/TaxCalculator.java
@@ -0,0 +1,8 @@
+public class TaxCalculator {
+
+    public double LBTT(double price){
+        //TODO: implemnt
+        double totalPrice = price;
+        return 0.0;
+    }
+}
